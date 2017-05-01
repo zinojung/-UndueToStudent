c070e3b4659a7606cb5c62e61a5c2ee7b2b152dc
정진호 <46firstmate@gmail.com>
2017-05-01 22:21:56 +0900

    spring boot init

---
+++ src/main/java/com/zino/undue/com/zino/undue/App.java
@@ -0,0 +1,13 @@
+package com.zino.undue.com.zino.undue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
